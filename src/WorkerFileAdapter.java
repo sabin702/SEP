b7bfbbd2e915a816aa1514ec11e22ffa28067c26
kreso11 <43003487+kreso11@users.noreply.github.com>
2018-12-11 18:27:13 +0100

    Adapter simple
    Without everything

---
+++ src/WorkerFileAdapter.java
@@ -0,0 +1,59 @@
+import  java.io.FileNotFoundException;
+import java.io.IOException;
+
+
+public class WorkerFileAdapter
+{
+   private MyFileIO mifo;
+   private String fileName;
+   
+   public WorkerFileAdapter(String fileName)
+   {
+      mifo=new MyFileIO();
+      this.fileName=fileName;
+   }
+   
+   public WorkersList getAllWorkers()
+   {
+      WorkersList workers=new WorkersList();
+      
+      try
+      {
+         workers= (WorkersList)mifo.readObjectFromFile(fileName);
+      }
+      
+      catch (FileNotFoundException e)
+      {
+         System.out.println("File not found");
+      }
+      catch (IOException e)
+      {
+         System.out.println("IO error reading files");
+      }
+      catch (ClassNotFoundException e)
+      {
+         System.out.println("Class not found");
+      }
+      
+      return workers;
+   }
+   
+   public void saveWorkers(WorkersList workers)
+   {
+      try
+      {
+         mifo.writeToFile(fileName, workers);
+      }
+      catch (FileNotFoundException e)
+      {
+            System.out.println("File not found");
+      }
+      catch (IOException e)
+      {
+         System.out.println("IO Error writing to file");
+      }
+   }
+   
+   public void changeWorkerName()
+   
+}
