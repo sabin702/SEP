aa5d81ff112d6d6a2744620ba6b2590d3e8edd03
Sabin <35765016+xplameboy@users.noreply.github.com>
2018-12-15 17:42:40 +0100

    WeeklyPlan Classes
    I created the weeklyPlan class, list and adapter

--- src/WeeklyPlan.java
+++ src/WeeklyPlan.java
@@ -1,5 +1,78 @@
-
 public class WeeklyPlan
 {
-
+   private Analysis analysis;
+   private String weekSize;
+   //private double numberOfEmployees;
+   double[] weeklyEmployees;
+   
+   public WeeklyPlan(Analysis analysis, String weekSize) {
+      this.analysis = analysis;
+      this.weekSize = weekSize;
+      //this.numberOfEmployees = numberOfEmployees;
+      weeklyEmployees = new double[6];
+   }
+   
+   public Analysis getAnalysis() {
+      return analysis;
+   }
+   
+   public String getWeekSize() {
+      return weekSize;
+   }
+   
+   public void setWeekSize(String weekSize) {
+      this.weekSize = weekSize;
+   }
+   
+   public double getNumberOfEmployees(int index) {
+      //return numberOfEmployees;
+      return weeklyEmployees[index];
+   }
+   
+   public void setNumberOfEmployees(int index, double numberOfEmployees) {
+      weeklyEmployees[index] = numberOfEmployees;
+   }
+   
+   public String getWeeklyEmployees() {
+      String str = "";
+      for (int i = 0; i < weeklyEmployees.length; i++)
+      {
+         str += " " +  weeklyEmployees[i];
+      }
+      return str;
+   }
+   
+   public double[] getWeeklyEmployeesArray() {
+      return weeklyEmployees;
+   }
+   
+   public boolean equalsWeeklyEmployees(double[] temp) {
+      if (temp.length != weeklyEmployees.length)
+         return false;
+      for (int i = 0; i < temp.length; i++)
+      {
+         if (temp[i] != weeklyEmployees[i])
+            return false;
+      }
+      return true;
+   }
+   
+   public String toString() {
+      return analysis.getMatrix() + " " 
+   + weekSize + " " 
+   + analysis.getAnalysisType() + " " 
+   + getWeeklyEmployees();
+   }
+   
+   public boolean equals(Object obj) {
+      if (!(obj instanceof WeeklyPlan))
+         return false;
+      
+      WeeklyPlan temp = (WeeklyPlan) obj;
+      return analysis.equals(temp.getAnalysis()) 
+            && weekSize.equals(temp.getWeekSize())
+            && equalsWeeklyEmployees(temp.getWeeklyEmployeesArray());
+   }
+   
+   
 }
---
+++ src/WeeklyPlanList.java
@@ -0,0 +1,70 @@
+import java.util.ArrayList;
+
+public class WeeklyPlanList
+{
+   ArrayList<WeeklyPlan> weeklyPlans;
+   
+   public WeeklyPlanList() {
+      weeklyPlans = new ArrayList<WeeklyPlan>();
+   }
+   
+   public void addWeeklyPlan(WeeklyPlan weeklyPlan) {
+      weeklyPlans.add(weeklyPlan);
+   }
+   
+   public void remove(WeeklyPlan weeklyPlan) {
+      weeklyPlans.remove(weeklyPlan);
+   }
+   
+   public void remove(int index) {
+      weeklyPlans.remove(index);
+   }
+   
+   public int getIndex(Analysis analysis, String weekSize, double[] weeklyNumber) 
+   {
+      for(int i = 0;i<weeklyPlans.size();i++) {
+         WeeklyPlan temp = weeklyPlans.get(i);
+         
+         if (temp.getAnalysis().equals(analysis) 
+               && temp.getWeekSize().equals(weekSize)
+               && temp.equalsWeeklyEmployees(weeklyNumber))
+            return i;
+      }
+      return -1;
+   }
+   
+   public WeeklyPlan get(int index) {
+      if (index<weeklyPlans.size())
+         return weeklyPlans.get(index);
+      else
+         return null;
+   }
+   
+   public int size() {
+      return weeklyPlans.size();
+   }
+   
+   public void set(WeeklyPlan weeklyPlan, int index) {
+      weeklyPlans.set(index, weeklyPlan);
+   }
+   
+   public String toString() {
+      String str = "";
+      for (int i = 0; i < weeklyPlans.size(); i++)
+      {
+         WeeklyPlan temp = weeklyPlans.get(i);
+         
+         str += temp + "\n";
+      }
+      return str;
+   }
+   
+   public boolean equals(Object obj) {
+      if (!(obj instanceof WeeklyPlanList))
+         return false;
+      WeeklyPlanList  temp = (WeeklyPlanList) obj;
+      if (temp.size() != size())
+         return false;
+      return weeklyPlans.equals(temp.weeklyPlans);
+   }
+}
