25c3e418042fa818f7b343da4594c4187f373481
Sabin <35765016+xplameboy@users.noreply.github.com>
2018-12-16 22:45:28 +0100

    Update WorkPlan

--- src/WorkPlan.java
+++ src/WorkPlan.java
@@ -1,5 +1,60 @@
+import java.io.Serializable;
+import java.util.ArrayList;
 
-public class WorkPlan
+public class WorkPlan implements Serializable
 {
+   private Worker worker;
+   private TrainingList trainings;
+   private AnnualPerformance annualPerformance;
+   private ArrayList<MyDate> dates;
+   
+   public WorkPlan(Worker worker, AnnualPerformance annualPerformance, TrainingList trainings) {
+      this.worker = worker;
+      this.annualPerformance = annualPerformance;
+      this.trainings = trainings;
+      dates = new ArrayList<MyDate>();
+   }
+   
+   public Worker getWorker() {
+      return worker;
+   }
+   
+   public AnnualPerformance getAnnualPerformance() {
+      return annualPerformance;
+   }
+   
+   public Analysis getAnalysis(int index) {
+      return trainings.getTraining(index).getAnalysis();
+   }
+   
+   public TrainingList getTrainings() {
+      return trainings;
+   }
+   
+   public String getAnalysis() {
+      String str = "";
+      for (int i = 0; i < trainings.getSize(); i++) {
+         str += trainings.getTraining(i).getAnalysis().getAnalysisType()
+               + " (" + trainings.getTraining(i).getAnalysis().getMatrix() + ") and ";
+      }
+      return str;
+   }
+   
+   public void setTrainings(TrainingList trainings) {
+      this.trainings = trainings;
+   }
+   
+   public String toString() {
+      return worker + " " + getAnalysis();
+   }
+   
+   public boolean equals(Object obj) {
+      if(!(obj instanceof WorkPlan))
+         return false;
+      WorkPlan temp = (WorkPlan) obj;
+      return worker.equals(temp.getWorker()) 
+            && annualPerformance.equals(temp.getAnnualPerformance())
+            && trainings.equals(temp.trainings);
+   }
    
 }
